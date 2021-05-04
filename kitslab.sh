361d6a6a6e302137d316a63db2fbb418fe01c753
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2021-05-04 13:00:05 +0200

    Dotfiles

--- kitslab.sh
+++ kitslab.sh
@@ -10,7 +10,7 @@ export PS1="%n@%m : %~ => "
 
 autoload -U colors && colors
 if [[ $(hostname -s) = w-* ]]; then
-  PS1="%{$fg[white]%}%n%{$reset_color%}@%{$bg[white]$fg[black]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}> "
+    PROMPT="%{$fg[white]%}%n%{$reset_color%}@%{$bg[white]$fg[black]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}%{$fg[white]%}█{$reset_color%}'\n' > "
 elif [[ $(hostname -s) = b-* ]]; then
   PS1="%{$fg[white]%}%n%{$reset_color%}@%{$bg[blue]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}> "
 elif [[ $(hostname -s) = g-* ]]; then
