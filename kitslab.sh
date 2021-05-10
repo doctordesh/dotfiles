67c4b0be21607fc3f6be7b67f9c17a3696b0c53b
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2021-05-10 15:21:48 +0200

    WIP

--- kitslab.sh
+++ kitslab.sh
@@ -9,12 +9,15 @@ export PS1="%n@%m : %~ => "
 
 
 autoload -U colors && colors
-if [[ $(hostname -s) = w-* ]]; then
-    PROMPT="%{$fg[white]%}%n%{$reset_color%}@%{$bg[white]$fg[black]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}%{$fg[white]%}█{$reset_color%}'\n' > "
-elif [[ $(hostname -s) = b-* ]]; then
-  PS1="%{$fg[white]%}%n%{$reset_color%}@%{$bg[blue]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}> "
-elif [[ $(hostname -s) = g-* ]]; then
-  PS1="%{$fg[white]%}%n%{$reset_color%}@%{$bg[green]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}> "
+if [[ $(hostname -s) = w* ]]; then
+    PROMPT="%{$fg[green]%}%n%{$fg[red]%}@%{$fg[white]%}%m %{$fg[blue]%}%~%{$reset_color%}"$'\n'"%{$fg[magenta]%}>%{$reset_color%} "
+    # PROMPT="%{$fg[white]%}%n%{$reset_color%}@%{$bg[white]$fg[black]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}%{$fg[white]%}█%{$reset_color%} > "
+elif [[ $(hostname -s) = b* ]]; then
+    PROMPT="%{$fg[green]%}%n%{$fg[red]%}@%{$fg[blue]%}%m %{$fg[blue]%}%~%{$reset_color%}"$'\n'"%{$fg[magenta]%}>%{$reset_color%} "
+#  PS1="%{$fg[white]%}%n%{$reset_color%}@%{$bg[blue]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}> "
+elif [[ $(hostname -s) = g* ]]; then
+    PROMPT="%{$fg[green]%}%n%{$fg[red]%}@%{$fg[green]%}%m %{$fg[blue]%}%~%{$reset_color%}"$'\n'"%{$fg[magenta]%}>%{$reset_color%} "
+ #   PS1="%{$fg[white]%}%n%{$reset_color%}@%{$bg[green]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}> "
 fi
 #colors
 
