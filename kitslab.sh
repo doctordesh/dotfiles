bd2443f99370e84c7d760bd10eaea4f3a5dc68eb
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2021-05-31 11:00:10 +0200

    Dotfiles

--- kitslab.sh
+++ kitslab.sh
@@ -11,13 +11,10 @@ export PS1="%n@%m : %~ => "
 autoload -U colors && colors
 if [[ $(hostname -s) = w* ]]; then
     PROMPT="%{$fg[green]%}%n%{$fg[red]%}@%{$fg[white]%}%m %{$fg[red]%}| %{$fg[blue]%}%~%{$reset_color%}"$'\n'"%{$fg[magenta]%}>%{$reset_color%} "
-    # PROMPT="%{$fg[white]%}%n%{$reset_color%}@%{$bg[white]$fg[black]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}%{$fg[white]%}█%{$reset_color%} > "
 elif [[ $(hostname -s) = b* ]]; then
     PROMPT="%{$fg[green]%}%n%{$fg[red]%}@%{$fg[blue]%}%m %{$fg[red]%}| %{$fg[blue]%}%~%{$reset_color%}"$'\n'"%{$fg[magenta]%}>%{$reset_color%} "
-#  PS1="%{$fg[white]%}%n%{$reset_color%}@%{$bg[blue]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}> "
 elif [[ $(hostname -s) = g* ]]; then
     PROMPT="%{$fg[green]%}%n%{$fg[red]%}@%{$fg[green]%}%m %{$fg[red]%}| %{$fg[blue]%}%~%{$reset_color%}"$'\n'"%{$fg[magenta]%}>%{$reset_color%} "
- #   PS1="%{$fg[white]%}%n%{$reset_color%}@%{$bg[green]%}%m%{$reset_color%} %{$fg[yellow]%}%~ %{$reset_color%}> "
 fi
 #colors
 
@@ -31,6 +28,11 @@ alias l="ls -lah"
 alias goenv="export PATH=$PATH:/usr/local/go/bin"
 alias em="emacs -nw @$"
 alias resource="source ~/.zshrc"
+
+function sardenv() {
+    source /opt/conda/etc/profile.d/conda.sh
+    conda activate sardana
+}
 
 function colors() {
     text="xYz"; # Some test text
