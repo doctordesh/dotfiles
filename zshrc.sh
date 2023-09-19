e9bf67456b752696a8c234c28a17f2900211d677
Emil Rosendahl <emil.rosendahl@maxiv.lu.se>
2023-09-19 12:00:05 +0200

    Dotfiles

--- zshrc.sh
+++ zshrc.sh
@@ -56,6 +56,7 @@ alias org="/Applications/EmacsOrg.app/Contents/MacOS/Emacs &"
 alias dockertangodevice="docker exec -it -e DISPLAY=`hostname`:0 device /bin/bash"
 alias foodtruck="curl -s http://w-v-kitslab-web-0.maxiv.lu.se:1810/foodtruck/today | python3 -m json.tool"
 alias taiga="docker kill taiga-is-awful; docker start taiga-is-awful"
+alias ca="conda activate"
 
 function eq() {
     PID=`ps -ef | grep 'eqMac.app' | awk \{'print$2'\} | head -n 1`
